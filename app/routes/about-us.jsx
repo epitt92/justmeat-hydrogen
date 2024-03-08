68ff67190cf5a60ca43ad53639d3c8b927f53fb5
Habibur Rahman <habiburrahman0540@gmail.com>
2024-03-09 03:24:04 +0600

    Added aboutus page and recipes page

---
+++ app/routes/about-us.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AboutusVideo from "../components/AboutusPage/AboutusVideo";
+import Passion from "../components/AboutusPage/Passion";
+const Aboutus = () => {
+  return (
+    <>
+    <AboutusVideo/>
+    <Passion/>
+    </>
+  )
+}
+
+export default Aboutus
