34a4555f6b68adc890c5a18a71a2c62e43a50592
Habibur Rahman <habiburrahman0540@gmail.com>
2024-03-12 23:06:12 +0600

    Added privacy-policy page

---
+++ app/routes/privacy-policy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PrivacyPolicy from "../components/PrivacyPolicy/PrivacyPolicy";
+const PrivacyPolicyPage = () => {
+  return (
+    <>
+    <PrivacyPolicy/>
+    </>
+  )
+}
+
+export default PrivacyPolicyPage
