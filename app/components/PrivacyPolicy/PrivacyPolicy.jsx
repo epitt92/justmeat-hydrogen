07238875c7adcf00b8e72225035728ac08fb8d84
Habibur Rahman <habiburrahman0540@gmail.com>
2024-03-13 01:03:09 +0600

    Added upto Disclose Personal Information to privacy-policy page

--- app/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ app/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -30,9 +30,42 @@ const PrivacyPolicy = () => {
                                         Some features of the Services may require you to directly provide us with certain information about yourself. You may elect not to provide this information, but doing so may prevent you from using or accessing these features.</li>
                     </ul>
                 </p>
-            
-            
-            
+                <h2 className='font-Roboto text-[42px]  font-medium  mb-3'>Information We Collect through Cookies</h2>
+                <p className='my-3'>We also automatically collect certain information about your interaction with the Services ("<strong>Usage Data</strong>"). To do this, we may use cookies, pixels and similar technologies ("<strong>Cookies</strong>"). Usage Data may include information about how you access and use our Site and your account, including device information, browser information, information about your network connection, your IP address and other information regarding your interaction with the Services.</p>
+                <h2 className='font-Roboto text-[42px]  font-medium  mb-3'>Information We Obtain from Third Parties</h2>
+                <p className='my-3'>Finally, we may obtain information about you from third parties, including from vendors and service providers who may collect information on our behalf, such as:</p>
+                <p>
+                    <ul className="list-disc ">
+                        <li className='mb-3'>Companies who support our Site and Services, such as Shopify.</li>
+                        <li className='mb-3'>Our payment processors, who collect payment information (e.g., bank account, credit or debit card information, billing address) to process your payment in order to fulfill your orders and provide you with products or services you have requested, in order to perform our contract with you.</li>
+                        <li className='mb-3'>When you visit our Site, open or click on emails we send you, or interact with our Services or advertisements, we, or third parties we work with, may automatically collect certain information using online tracking technologies such as pixels, web beacons, software developer kits, third-party libraries, and cookies.
+                                            Any information we obtain from third parties will be treated in accordance with this Privacy Policy. We are not responsible or liable for the accuracy of the information provided to us by third parties and are not responsible for any third party's policies or practices. For more information, see the section below, Third Party Websites and Links.</li>
+                         </ul>
+                </p>
+                <h2 className='font-Roboto text-[42px]  font-medium  mb-3'>How We Use Your Personal Information</h2>
+                <p>
+                    <ul className="list-disc ">
+                        <li className='mb-3'><strong>Providing Products and Services.</strong> We use your personal information to provide you with the Services in order to perform our contract with you, including to process your payments, fulfill your orders, to send notifications to you related to you account, purchases, returns, exchanges or other transactions, to create, maintain and otherwise manage your account, to arrange for shipping, facilitate any returns and exchanges and to enable you to post reviews.</li>
+                        <li className='mb-3'><strong>Marketing and Advertising.</strong> We use your personal information for marketing and promotional purposes, such as to send marketing, advertising and promotional communications by email, text message or postal mail, and to show you advertisements for products or services. This may include using your personal information to better tailor the Services and advertising on our Site and other websites.</li>
+                        <li className='mb-3'><strong>Security and Fraud Prevention. </strong>We use your personal information to detect, investigate or take action regarding possible fraudulent, illegal or malicious activity. If you choose to use the Services and register an account, you are responsible for keeping your account credentials safe. We highly recommend that you do not share your username, password, or other access details with anyone else. If you believe your account has been compromised, please contact us immediately.</li>
+                        <li className='mb-3'><strong>Communicating with you. </strong>We use your personal information to provide you with customer support and improve our Services. This is in our legitimate interests in order to be responsive to you, to provide effective services to you, and to maintain our business relationship with you.</li>
+                    </ul>
+                </p>
+                <h2 className='font-Roboto text-[42px]  font-medium  mb-3'>Cookies</h2>
+                <p className='my-3'>Like many websites, we use Cookies on our Site. For specific information about the Cookies that we use related to powering our store with Shopify, see https://www.shopify.com/legal/cookies. We use Cookies to power and improve our Site and our Services (including to remember your actions and preferences), to run analytics and better understand user interaction with the Services (in our legitimate interests to administer, improve and optimize the Services). We may also permit third parties and services providers to use Cookies on our Site to better tailor the services, products and advertising on our Site and other websites.</p>
+                <p className='my-3'>Most browsers automatically accept Cookies by default, but you can choose to set your browser to remove or reject Cookies through your browser controls. Please keep in mind that removing or blocking Cookies can negatively impact your user experience and may cause some of the Services, including certain features and general functionality, to work incorrectly or no longer be available. Additionally, blocking Cookies may not completely prevent how we share information with third parties such as our advertising partners.</p>
+                <h2 className='font-Roboto text-[42px]  font-medium  mb-3'>How We Disclose Personal Information</h2>
+                <p className='my-3'>In certain circumstances, we may disclose your personal information to third parties for legitimate purposes subject to this Privacy Policy. Such circumstances may include:</p>
+                <p>
+                    <ul className="list-disc ">
+                        <li className='mb-3'>With vendors or other third parties who perform services on our behalf (e.g., IT management, payment processing, data analytics, customer support, cloud storage, fulfillment and shipping).</li>
+                        <li className='mb-3'>With business and marketing partners, including Shopify, to provide services and advertise to you. Our business and marketing partners will use your information in accordance with their own privacy notices.</li>
+                        <li className='mb-3'>When you direct, request us or otherwise consent to our disclosure of certain information to third parties, such as to ship you products or through your use of social media widgets or login integrations, with your consent.</li>
+                        <li className='mb-3'>With our affiliates or otherwise within our corporate group, in our legitimate interests to run a successful business.</li>
+                        <li className='mb-3'>In connection with a business transaction such as a merger or bankruptcy, to comply with any applicable legal obligations (including to respond to subpoenas, search warrants and similar requests), to enforce any applicable terms of service, and to protect or defend the Services, our rights, and the rights of our users or others.
+We have, in the past 12 months disclosed the following categories of personal information and sensitive personal information (denoted by *) about users for the purposes set out above in "How we Collect and Use your Personal Information" and "How we Disclose Personal Information":</li>
+                    </ul>
+                </p>
             </div>
         
         </div>
