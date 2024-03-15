9b6a56e923675f8c8003eda63260993c8bc4302c
Habibur Rahman <habiburrahman0540@gmail.com>
2024-03-16 01:35:21 +0600

    rechargeUtils file update

--- app/lib/rechargeUtils.js
+++ app/lib/rechargeUtils.js
@@ -1,28 +1,36 @@
-import { loginWithShopifyCustomerAccount } from '@rechargeapps/storefront-client';
+import { loginShopifyApi } from '@rechargeapps/storefront-client';
 import { json } from '@shopify/remix-oxygen';
 
-const RECHARGE_SESSION_KEY = 'rechargeSession';
+// Recharge session is good for 60 minutes so set to 55 minutes to avoid race conditions
+const RECHARGE_SESSION_DURATION = 1000 * 60 * 55;
 
 // loginHelper function
-async function loginRecharge(context) {
-  const customerAccessToken = await context.customerAccount.getAccessToken();
-  const rechargeSession = await loginWithShopifyCustomerAccount(customerAccessToken);
-  console.log(rechargeSession);
+export async function loginRecharge({ hydrogenSession, shopifyStorefrontToken }) {
+  const customerAccessToken = await hydrogenSession.get('customerAccessToken');
+  const rechargeSession = await loginShopifyApi(shopifyStorefrontToken, customerAccessToken);
+
   if (rechargeSession) {
-    context.rechargeSession.set(RECHARGE_SESSION_KEY, rechargeSession);
+    const sessionWithExpires = {
+      ...rechargeSession,
+      expiresAt: Date.now() + RECHARGE_SESSION_DURATION,
+    };
+    await hydrogenSession.set('rechargeSession', sessionWithExpires);
   } else {
     // this should match your catch boundary
-    throw json('No session created *+****', { status: 400 });
+    throw json('No session created', { status: 400 });
   }
 
   return rechargeSession;
 }
 
 // helper function for data fetching
-export async function rechargeQueryWrapper(rechargeFn, context) {
-  let rechargeSession = context.rechargeSession.get(RECHARGE_SESSION_KEY);
-  if (!rechargeSession) {
-    rechargeSession = await loginRecharge(context);
+export async function rechargeQueryWrapper(rechargeFn, { hydrogenSession, shopifyStorefrontToken }) {
+  let rechargeSession = await hydrogenSession.get('rechargeSession');
+  if (!rechargeSession || rechargeSession.expiresAt < Date.now()) {
+    rechargeSession = await loginRecharge({
+      hydrogenSession,
+      shopifyStorefrontToken,
+    });
   }
 
   try {
@@ -31,7 +39,10 @@ export async function rechargeQueryWrapper(rechargeFn, context) {
     try {
       if (e?.status === 401) {
         // handle auth error, login again and retry request
-        rechargeSession = await loginRecharge(context);
+        rechargeSession = await loginRecharge({
+          hydrogenSession,
+          shopifyStorefrontToken,
+        });
         return await rechargeFn(rechargeSession);
       }
       // this should match your catch boundary
