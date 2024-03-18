c9a2db87e0f1ed07d17233491cff37a9b1ff00ab
Sufyan-Dev32 <113394135+FaixySaati@users.noreply.github.com>
2024-03-18 07:56:43 +0500

    Revert "update progress function"

--- app/components/ui/CustomProgressBar.jsx
+++ app/components/ui/CustomProgressBar.jsx
@@ -5,18 +5,19 @@ import { Progress } from './progress';
 
 const CustomProgressBar = ({cost}) => {
     const [progress, setProgress] = useState(0);
-    useEffect(() => {
-      if(cost >= 100){
-        setProgress(100);
-      }else{
+    // useEffect(() => {
+    //   if(cost >= 100){
+    //     setProgress(100);
 
-        setProgress(cost);
-      }
-      }, [cost])
+    //   }else{
+
+    //     setProgress(cost);
+    //   }
+    //   }, [cost])
 
       return ( 
-        <div className='py-4 px-7 flex justify-center'>
-            <Progress value={progress} className="w-[90%] min-h-3 border border-[#000]" />
+        <div className='py-4 px-7 pb-10 flex justify-center'>
+            <Progress value={cost >= 100 ? 100 : cost} className="w-[90%] min-h-3 border border-[#000]" />
         </div>
       )
 
--- app/components/ui/progress.jsx
+++ app/components/ui/progress.jsx
@@ -6,7 +6,6 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 import { cn } from "app/lib/utils"
 
 const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
-  
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
