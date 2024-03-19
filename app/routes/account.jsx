9ef6245f43036d526a1a1452ecab69303b98b650
Codelock Solutions <codelockinfo@gmail.com>
2024-03-20 02:02:56 +0530

    Changes for collections page

--- app/routes/account.jsx
+++ app/routes/account.jsx
@@ -83,7 +83,7 @@ export default function AccountLayout() {
       <h1>{heading}</h1>
       
       {subscriptionsResponse.subscriptions && (
-        <AccountSubscriptions subscriptions={subscriptionsResponse.subscriptions} />
+        <AccountSubscriptions subscriptions={subscriptionsResponse.subscriptions} currentcustomer={customer}/>
       )}
       <br />
       {/*<AccountMenu />*/}
@@ -128,22 +128,27 @@ function Logout() {
   );
 }
 
-function AccountSubscriptions({ subscriptions }) {
+function AccountSubscriptions({subscriptions, currentcustomer}) {
   return (
-    <div className="mt-6">
+    <div className="mt-6 bg-sublistbgGray">
       <div className="grid w-full gap-4 p-4 py-6 md:gap-8 md:p-8 lg:p-12">
-        <h2 className="font-bold text-lead">Active Subscriptions</h2>
-        {subscriptions?.length ? <Subscriptions subscriptions={subscriptions} /> : <EmptySubscriptions />}
+        <h2 className="font-bold text-lead text-[28px] text-center md:text-left">Your Subscriptions</h2>
+        <div className='grid bg-custombgGreen w-auto md:w-[300px] p-6 block text-white text-xl text-center md:text-left'>
+          <span>Next Order Processing On</span>
+          <span>{subscriptions[0].next_charge_scheduled_at}</span>
+        </div>
+        <hr className='border-gray-500 border-t-2'></hr>
+        {subscriptions?.length ? <Subscriptions subscriptions={subscriptions}  currentcustomer={currentcustomer}/> : <EmptySubscriptions />}
       </div>
     </div>
   );
 }
 
-function Subscriptions({ subscriptions }) {
+function Subscriptions({ subscriptions,currentcustomer }) {
   return (
-    <ul className="grid grid-flow-row grid-cols-1 gap-2 gap-y-6 md:gap-4 lg:gap-6 false sm:grid-cols-3">
+    <ul className="grid bg-white border border-2 border-custombgGreen">
       {subscriptions.map(subscription => (
-        <SubscriptionCard subscription={subscription} key={subscription.id} />
+        <SubscriptionCard subscription={subscription} currentcustomer={currentcustomer} key={subscription.id} />
       ))}
     </ul>
   );
