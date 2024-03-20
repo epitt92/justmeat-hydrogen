1816aa31d8d04f35c5e710ba78b7ff9c7e1af69b
Sufyan-Dev32 <113394135+FaixySaati@users.noreply.github.com>
2024-03-20 06:47:55 +0500

    updated progress bar

--- app/components/ui/CustomProgressBar.jsx
+++ app/components/ui/CustomProgressBar.jsx
@@ -3,22 +3,20 @@ import { Progress } from './progress';
 
 
 
-const CustomProgressBar = ({cost}) => {
-    // const [progress, setProgress] = useState(0);
+const CustomProgressBar = ({ cost }) => {
+  const progressValue = (cost / 150) * 100; // Calculate the progress value based on the total and the target amount
 
-      return ( 
-        <>
-        <div className='py-4 px-10 flex justify-center'>
-            <Progress value={cost >= 100 ? 100 : cost } className="w-[100%] min-h-3 border border-[#000]" />
-            {/* <Progress value={cost  } className="w-[100%] min-h-3 border border-[#000]" /> */}
-        </div>
-         <div className='flex flex-column justify-end gap-2  px-7 relative'>
-            <p className='text-center text-base flex flex-col'><span className='text-[16px] uppercase leading-normal'>$75 </span><span className='text-[11px] uppercase leading-normal'>Unlock Order </span></p>
-            <p className='text-base text-center flex flex-col'><span className='text-[16px] leading-normal'>$125 </span><span className='text-[11px] uppercase leading-normal'>Free Meat </span><span className='text-[11px] leading-normal'>(UNLOCKED AT $125)</span></p>
-          </div>
-        </>
-      )
-
+  return (
+    <>
+      <div className='py-4 px-10 flex justify-center'>
+        <Progress value={progressValue >= 100 ? 100 : progressValue} className="w-[100%] min-h-3 border border-[#000]" />
+      </div>
+      <div className='flex flex-column justify-end gap-2  px-7 '>
+        <p className='text-center text-base flex flex-col mr-3 relative' > <span className='w-[10px] h-[10px] bg-black rounded-[100%] absolute top-[-27px] left-[50%] translate-x-[-50%]  '></span> <span className='text-[16px] uppercase leading-normal'>$75 </span><span className='text-[11px] uppercase leading-normal'>Unlock Order </span></p>
+        <p className='text-base text-center flex flex-col mr-5 relative'><span className='w-[10px] h-[10px] bg-black rounded-[100%] absolute top-[-27px] left-[50%] translate-x-[-50%]  '></span><span className='text-[16px] leading-normal'>$125 </span><span className='text-[11px] uppercase leading-normal'>Free Meat </span><span className='text-[11px] leading-normal'>(UNLOCKED AT $125)</span></p>
+      </div>
+    </>
+  )
 }
 
 export default CustomProgressBar
