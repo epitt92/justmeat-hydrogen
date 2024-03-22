dc5abf83fc6451cc85c01aa4167bfee992764dac
raheemdev <codieshub@gmail.com>
2024-03-23 02:53:20 +0500

    feat: added floating cart badge in screen

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -98,6 +98,9 @@ export default function Product() {
           <div className="custom-collection-wrap">
             <CustomCollection col={customCollectionProducts} />
           </div>
+          <div className='fixed bottom-[12px] left-[50%] transform translate-x-[-50%] w-[90%] rounded-[12px] bg-[#AAAAAA] min-h-[50px] flex justify-center items-center'>
+            <p className='text-white text-[20px]'>Add $63.55 to Unlock Cart ($8.59)</p>
+          </div>
         </div>
       </div>
     </>
