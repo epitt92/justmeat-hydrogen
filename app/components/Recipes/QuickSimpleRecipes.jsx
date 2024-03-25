8394a51566b12d9e706b1315324c01eeeb0f294a
raheemdev <codieshub@gmail.com>
2024-03-26 01:10:19 +0500

    worked on some fixings

--- app/components/Recipes/QuickSimpleRecipes.jsx
+++ app/components/Recipes/QuickSimpleRecipes.jsx
@@ -30,7 +30,7 @@ const QuickSimpleRecipes = () => {
 
   return (
     <section className="bg-white text-black py-8">
-      <div className="w-[85%] mx-auto bg-[url('https://cdn.shopify.com/s/files/1/0555/1751/1961/files/Mask_Group_161.png')] bg-center text-white py-8">
+      <div className="w-[85%] max-w-[1170px] mx-auto bg-[url('https://cdn.shopify.com/s/files/1/0555/1751/1961/files/Mask_Group_161.png')] bg-center text-white py-8">
         <div className="mt-0 md:mt-[170px] text-center mb-0 md:mb-[42px] px-2 md:px-[66px] lg:px-[166px] pt-0 md:pt-[19px] pb-[1px]">
           <p>
             The hardest part of meal prepping has never been easier! Just Meats
@@ -41,7 +41,7 @@ const QuickSimpleRecipes = () => {
           </p>
         </div>
       </div>
-      <div className="w-[85%] mx-auto p-4">
+      <div className="w-[85%] max-w-[1170px] mx-auto p-4">
         <h1 className="font-Roboto text-3xl font-semibold mt-2 mb-4 leading-5">
           Quick & Simple Recipes
         </h1>
