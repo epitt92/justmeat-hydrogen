64df1bb7738dfa4bc0aaf9e7a688dd7f182c9c27
hkim940917 <hkim940917@gmail.com>
2024-04-04 18:41:25 +0100

    update eslint

--- app/components/OrderButton.jsx
+++ app/components/OrderButton.jsx
@@ -6,7 +6,12 @@ const OrderButton = ({ className }) => {
   return (
     <div className="flex orderBton">
       <NavLink end prefetch="intent" to="/products/custom-bundle">
-        <span className={cn('bg-[#862E1B] cursor-pointer text-[#fff] hover:bg-[#1d1d1d] transition font-bold	text-lg py-3 px-9', className)}>
+        <span
+          className={cn(
+            'bg-[#862E1B] cursor-pointer text-[#fff] hover:bg-[#1d1d1d] transition font-bold	text-lg py-3 px-9',
+            className,
+          )}
+        >
           Order Now
         </span>
       </NavLink>
