d81181c5c6ac18ef5d334cf2d8c118a9dd5dae5b
beachstrider <mernstackdev729@gmail.com>
2024-04-07 11:39:07 -0600

    add: how it works section wrapper

---
+++ app/containers/MayhemMadness/HowItWorks.jsx
@@ -0,0 +1,9 @@
+export const HowItWorks = () => {
+  return (
+    <section className="bg-[#222222] text-white sm:pt-[100px] sm:pb-[110px] pt-[50px] pb-[60px]">
+      <div className="text-[14px] font-normal tracking-[3px] text-center">
+        HOW IT WORKS SECTION HERE
+      </div>
+    </section>
+  )
+}
