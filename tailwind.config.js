4978eb4a3f6afd868e70908e5e7fabe414e9584e
hkim917 <hkim940917@gmail.com>
2024-04-07 05:39:27 +0100

    add tutorial slider in homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 import formsPlugin from '@tailwindcss/forms'
 import typographyPlugin from '@tailwindcss/typography'
 
+import withMT from '@material-tailwind/react/utils/withMT'
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+export default withMT({
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    'node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+    'node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+  ],
   plugins: [formsPlugin, typographyPlugin],
   theme: {
     fontFamily: {
@@ -39,4 +45,4 @@ export default {
       },
     },
   },
-}
+})
