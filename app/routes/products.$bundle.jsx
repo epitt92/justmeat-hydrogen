766ac5d5e55f51ca1d86fdd06e2cc7e4fd208e88
Raza Imran <razaimrankhan@gmail.com>
2024-04-09 04:57:40 +0500

    design changes and optimization

--- app/routes/products.$bundle.jsx
+++ app/routes/products.$bundle.jsx
@@ -118,7 +118,7 @@ export default function Product() {
   return (
     <>
       <Notification />
-      <div className="bg-cover h-[100%] w-[100%] bg-fixed	flex justify-center sm:bg-house">
+      <div className="bg-cover h-[100%] w-[100%] bg-fixed	flex justify-center sm:bg-house bg-[url(https://cdn.shopify.com/s/files/1/0555/1751/1961/files/Mask_Group_125_2_1.png?v=1693548433)]">
         <div className="container">
           <CustomBundle />
         </div>
