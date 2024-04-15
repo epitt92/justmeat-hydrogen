1137e2d6001756d388203bc4488fa852eefa7b1f
Raza Imran <razaimrankhan@gmail.com>
2024-04-15 22:19:09 +0500

    Critical changes raza

--- app/containers/CustomBundle/Cart/CartLines.jsx
+++ app/containers/CustomBundle/Cart/CartLines.jsx
@@ -19,7 +19,7 @@ export function CartLines() {
   return (
     <div
       aria-labelledby="cart-lines"
-      className="sm:h-[360px] overflow-auto px-[10px]"
+      className="sm:h-[360px] overflow-auto px-[10px] mb-[25px]"
     >
       <div className="grid grid-cols-3 gap-2 sm:grid-cols-1">
         {totalCost > 125 && <CartLineItem line={bonusLine} lineType="bonus" />}
