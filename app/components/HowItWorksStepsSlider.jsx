a85c9fbb3b7b9a84909f9ec98756110612e811ca
Habibur Rahman <habiburrahman0540@gmail.com>
2024-04-22 12:43:58 +0600

    Fixed secondinfo section

--- app/components/HowItWorksStepsSlider.jsx
+++ app/components/HowItWorksStepsSlider.jsx
@@ -45,7 +45,6 @@ export const HowItWorksStepsSlider = () => {
     <div className="relative flex justify-center overflow-x-hidden sm:mb-[56px] mb-[10px] sm:max-w-full max-w-[425px]">
       <div className="md:w-[600px] lg:w-[891px] w-full mx-auto">
         <Swiper
-          loop
           autoplay
           pagination={{ clickable: true }}
           slidesPerView={3}
