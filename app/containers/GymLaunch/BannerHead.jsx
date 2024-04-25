c969dd6aef89f17d86ce79c93f0a319bdf6b3354
Raza Imran <razaimrankhan@gmail.com>
2024-04-26 00:56:12 +0500

    Ui Fixes banner

--- app/containers/GymLaunch/BannerHead.jsx
+++ app/containers/GymLaunch/BannerHead.jsx
@@ -6,19 +6,19 @@ import JustMeatsLogo from '~/assets/images/justmeatslogoWhite.png'
 
 export const BannerHead = () => {
   return (
-    <section className="bg-[url('../assets/images/Gym_Banner1.png')] [background-size:110%] bg-black bg-right">
+    <section className="bg-[url('../assets/images/Gym_Banner1.png')] sm:[background-size:110%] [background-size:250%] bg-black bg-right bg-no-repeat">
         <div className="bg-[url('../assets/images/fade-gym_launch.png')] bg-no-repeat bg-transparent bg-left [background-size:80%_100%]">
         <div className='w-[80%] mx-auto'>
             <div className='flex flex-col md:flex-row pt-3'>
                 <div className='basis-2/2'>
                     <div className='flex flex-col-reverse md:flex-col md:items-left md:pb-0 pb-[80px]'>
-                        <div className="w-full md:w-[100%] h-auto  lg:pt-[100px] md:pt-[50px]">
+                        <div className="w-full md:w-[100%] h-auto  lg:pt-[100px] md:pt-[50px] max-w-[70%] sm:max-w-[100%] m-auto">
                             <img
                                 className=""
                                 src={GymLaunch}
                             />
                         </div>
-                        <div className="w-full md:w-[100%] h-auto  lg:pt-[50px] md:pt-[50px]">
+                        <div className="w-full md:w-[100%] h-auto  lg:pt-[50px] md:pt-[50px] max-w-[70%] sm:max-w-[100%] m-auto mb-[30px]">
                             <img
                                 className=""
                                 src={JustMeatsLogo}
@@ -28,7 +28,7 @@ export const BannerHead = () => {
                             <div className="font-nunito font-bold sm:text-[27px] text-[20px] text-yellow">
                             6 lbs of meat FOR FREE!
                             </div>
-                            <div className="sm:text-[36px] text-[27px] font-bold text-white leading-tight md:pb-[150px]">
+                            <div className="sm:text-[36px] font-bold text-white sm:leading-tight md:pb-[150px] text-[22px] [word-spacing:4px] sm:[word-spacing:0] leading-[42px]">
                             FOR FREE GYM LAUNCH
                                 <br />
                             SECRETS EXCLUSIVE DEAL!
